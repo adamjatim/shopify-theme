64901a0dbb8e5ee9b3fff0858cd329aed43f3352
adamjatim <adamrahmatilahi@gmail.com>
2023-01-29 22:50:56 +0700

    update hamburger style

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,11 @@ const navbarMobile = document.querySelector('.navbar-mobile');
 
 hamburger.addEventListener('click', function () {
     navbarMobile.classList.toggle('is-active');
-})
+});
+
+const hamburgerCancel = document.querySelector('.hamburger-cancel');
+const activeNavbarMobile = document.querySelector('.navbar-mobile.is-active')
+
+hamburgerCancel.addEventListener('click', function () {
+    activeNavbarMobile.classList.remove('is-active');
+});
