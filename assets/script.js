59bda0efe9f01ae89c0195bd1c9e3c552a755b86
adamjatim <adamrahmatilahi@gmail.com>
2023-02-20 00:08:29 +0700

    update .minicart script.js

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,20 @@ const hamburgerCancel = document.querySelector('.hamburger-cancel');
 // minicart
 const cart = document.getElementById('cart');
 const minicart = document.querySelector('.minicart');
+const minicartCancel = document.querySelector('.cart-cancel');
 
 hamburger.addEventListener('click', function () {
     navbarMobile.classList.toggle('is-active');
+});
+
+hamburgerCancel.addEventListener('click', function () {
+    navbarMobile.classList.remove('is-active');
 });
 
 cart.addEventListener('click', function () {
     minicart.classList.toggle('is-active');
 });
 
-hamburgerCancel.addEventListener('click', function () {
-    navbarMobile.classList.remove('is-active');
-    minicart.classList.remove('is-active');
-});
+minicartCancel.addEventListener('click', function () {
+    minicart.classList.remove('is-active');    
+})
