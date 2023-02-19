6b42573f611f2a3d2761e94eba5528299097a35c
adamjatim <adamrahmatilahi@gmail.com>
2023-02-19 23:58:36 +0700

    update .minicart script.js

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,22 @@
-// hamburger function
+// hamburger
 const hamburger = document.querySelector('.hamburger');
 const navbarMobile = document.querySelector('.navbar-mobile');
+
+const hamburgerCancel = document.querySelector('.hamburger-cancel');
+
+// minicart
+const cart = document.getElementById('cart');
+const minicart = document.querySelector('.minicart');
 
 hamburger.addEventListener('click', function () {
     navbarMobile.classList.toggle('is-active');
 });
 
-const hamburgerCancel = document.querySelector('.hamburger-cancel');
-const activeNavbarMobile = document.querySelector('.navbar-mobile.is-active')
+cart.addEventListener('click', function () {
+    minicart.classList.toggle('is-active');
+});
 
 hamburgerCancel.addEventListener('click', function () {
     navbarMobile.classList.remove('is-active');
+    minicart.classList.remove('is-active');
 });
-
-// minicart function
-const cart = document.getElementById('cart');
-const minicart = document.querySelector('.minicart');
-
-cart.addEventListener('click', function () {
-    minicart.classList.toggle('is-active');
-})
