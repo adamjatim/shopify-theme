1e51edf8a2054b792ab704abb72143497cca795b
adamjatim <adamrahmatilahi@gmail.com>
2023-02-24 07:57:57 +0700

    update minicart statment

--- assets/script.js
+++ assets/script.js
@@ -72,3 +72,28 @@ document.getElementById("add-product-form").addEventListener("submit", function(
   });
 });
 
+// Get all of the plus and minus buttons
+const plusButtons = document.querySelectorAll('.plus-btn');
+const minusButtons = document.querySelectorAll('.minus-btn');
+
+// Loop through the plus buttons and add event listeners
+plusButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the quantity input field for this button
+    const input = button.parentElement.querySelector('input[type="number"]');
+    
+    // Increase the quantity by one
+    input.stepUp();
+  });
+});
+
+// Loop through the minus buttons and add event listeners
+minusButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the quantity input field for this button
+    const input = button.parentElement.querySelector('input[type="number"]');
+    
+    // Decrease the quantity by one
+    input.stepDown();
+  });
+});
