620ad59e913a8afa9ffa38cca8c5a62711d0446b
adamjatim <adamrahmatilahi@gmail.com>
2023-02-28 17:56:36 +0700

    test product option

--- assets/script.js
+++ assets/script.js
@@ -34,26 +34,6 @@ for (var i = 0; i < radios.length; i++) {
   });
 }
 
-var variantRadio = document.querySelectorAll('.product-variant');
- variantRadio.forEach(function(el) {
-  el.style.display = '';
-});
-
-
-// function updateParentObject(radio) {
-//   var parentObject = document.getElementsByClassName('.product-variant');
-//   if (radio.value === 'option1') {
-//     // Code to update the parent object when option 1 is selected
-//     parentObject.style.backgroundColor = 'red';
-//   } else if (radio.value === 'option2') {
-//     // Code to update the parent object when option 2 is selected
-//     parentObject.style.backgroundColor = 'green';
-//   } else if (radio.value === 'option3') {
-//     // Code to update the parent object when option 3 is selected
-//     parentObject.style.backgroundColor = 'blue';
-//   }
-// }
-
 // hold product-page after add to cart
 document.getElementById("add-product-form").addEventListener("submit", function(event) {
   // Prevent the default form submission action
@@ -71,31 +51,32 @@ document.getElementById("add-product-form").addEventListener("submit", function(
   });
 });
 
-// hold minicart after increment and decrement value
-document.getElementById('plus-minus-btn').addEventListener("submit", function(event) {
-  // Prevent the default form submission action
-  event.preventDefault();
-  
-  // Get the form data
-  var formData = new FormData(event.target);
-  
-  // Do something with the form data, such as submit it using AJAX
-  fetch(event.target.action, {
-    method: event.target.method,
-    body: formData
-  }).then(function() {
-    console.log("Form submitted successfully!");
+var product = {{ product | json }};
+document.querySelectorAll('.product-variant input[type="radio"]').forEach(radio => {
+  radio.addEventListener('change', () => {
+    // find selection product
+    var selectVariants = [];
+
+    document.querySelectorAll('.product-variant input[type="radio"]:checked').forEach(radio => {
+      selectVariants.push(radio.value);
+    });
+    
+    // finding matched variant
+    var matchedVariant = product.variants.find(variant => {
+      for(var i = 0; i < selectVariants.length; i++) {
+        var pass = true;
+        if(selectVariants.indexOf(variant.options[i]) === -1){
+          pass = false;
+          break;
+        }
+      }
+      return pass;
+    })
+    // change product form variant id
+    document.querySelector('#product-id').value = matchedVariant.id;
+
+    // change url
+    var url = new URLParse(window.location.href, true);
+    console.log(url);
   });
 });
-
-  function incrementNum(key) {
-    var element = document.getElementById('updates_' + key);
-    element.stepUp();
-  }
-
-  function decrementNum(key) {
-    var element = document.getElementById('updates_' + key);
-    if (element.value > 1) {
-      element.stepDown();
-    }
-  }
