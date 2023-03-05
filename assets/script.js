f07ca126f8277e85fc628eaa5a960cf0b99b8362
adamjatim <adamrahmatilahi@gmail.com>
2023-03-06 05:41:09 +0700

    update product-price when click variant

--- assets/script.js
+++ assets/script.js
@@ -50,23 +50,3 @@ for (var i = 0; i < radios.length; i++) {
 //     console.log("Form submitted successfully!");
 //   });
 // });
-
-var decodeEntities = (function() {
-  // this prevents any overhead from creating the object each time
-  var element = document.createElement('div');
-
-  function decodeHTMLEntities (str) {
-    if(str && typeof str === 'string') {
-      // strip script/html tags
-      str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
-      str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
-      element.innerHTML = str;
-      str = element.textContent;
-      element.textContent = '';
-    }
-
-    return str;
-  }
-
-  return decodeHTMLEntities;
-})();
